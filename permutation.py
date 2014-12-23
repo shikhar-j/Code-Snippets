a2b7003ecfa61337087ab6c288795a075e0daef8
Shikhar Jaiswal <mail@shikharjaiswal.com>
2014-12-23 17:31:47 +0530

    nothing

--- permutation.py
+++ permutation.py
@@ -5,7 +5,6 @@ def permutations(string, step = 0):
     for i in range(step, len(string)):
         string_copy = [character for character in string]
         string_copy[step], string_copy[i] = string_copy[i], string_copy[step]
-        print string_copy, string, step
 
         permutations(string_copy, step + 1)
 
